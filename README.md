deb5db74c58b0065f76107efc3ff502584d9567f
huchuanzhao <330906552@qq.com>
2017-11-15 14:16:47 +0800

    modify drawing function

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # DjangoPrimer: minimum django(Django1.11.3)
-this *.py contains what the minimum django needs
+This *.py contains what the minimum django needs
 1. setting.py
 2. urls.py
 3. views.
--- templateHello.py
+++ templateHello.py
@@ -7,66 +7,23 @@
 
 import os
 import sys
-from django.shortcuts import render,render_to_response
-from django.http import HttpResponse, HttpResponseBadRequest
-from django.conf.urls import url
-from django.conf import settings
-from django.core.wsgi import get_wsgi_application
-from django import forms
-from PIL import Image, ImageDraw
+import hashlib
 from io import BytesIO
+from PIL import Image, ImageDraw
+from django import forms
+from django.conf import settings
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
 from django.core.cache import cache
 from django.core.urlresolvers import reverse
-
-class ImageForm(forms.Form):
-    height = forms.IntegerField(max_value=2000,min_value=1)
-    width = forms.IntegerField(max_value=2000,min_value=1)
-    def generate(self, image_formate='png'):
-        height = self.cleaned_data['height']
-        width = self.cleaned_data['width']
-        text = '{}.{}.{}'.format(width, height, image_formate)
-        content = cache.get(text)
-        if content is None:
-            image = Image.new('RGB', (width, height))
-            draw = ImageDraw.Draw(image)
-            textwidth,textheight = draw.textsize(text)
-            if textwidth < width and textheight < height:
-                texttop = (height - textheight) // 2
-                textleft = (width - textwidth) // 2
-                draw.text((textleft,texttop), text, fill=(255,255,255))
-            content = BytesIO()
-            image.save(content, image_formate)
-            content.seek(0)
-            cache.set(text, content, 60*60)
-        return content
-
-def placeholder(req, width, height):
-    form = ImageForm({'width':width, 'height':height})
-    if form.is_valid():
-        height = form.cleaned_data['height']
-        width = form.cleaned_data['width']
-        image = form.generate()
-        return HttpResponse(image, content_type='image/png')
-    else:
-        return HttpResponseBadRequest('图片格式错误')
-
-def index(req):
-    example = reverse('placeholder', kwargs={'width':50, 'height':50})
-    context = {
-        'example': req.build_absolute_uri(example)
-    }
-    # print ('********',context)
-    return render_to_response('home.html', context)
-
-urlpatterns=[
-    url(u'^$',index, name='homepage'),
-    url(u'^image/(?P<width>[0-9]+)*(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
-]
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render,render_to_response
+from django.views.decorators.http import etag
 
 DEBUG = os.environ.get('DEBUG','on')=='on'
-SECRET_KEY = os.environ.get('SECRET_KEY','&8x8ono))lhdi_6fg!h_9uv3l97w$m$(m6lg&0tttyb2e_lnlv')
+SECRET_KEY = os.environ.get('SECRET_KEY',
+                            '&8x8ono))lhdi_6fg!h_9uv3l97w$m$(m6lg&0tttyb2e_lnlv')
 ALLOWED_HOSTS=['*']
-# BASE_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(__file__)
 settings.configure(
     DEBUG=DEBUG,
@@ -85,7 +42,7 @@ settings.configure(
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
             'DIRS': [os.path.join(BASE_DIR, 'templates')],
-            'APP_DIRS': True,
+            # 'APP_DIRS': True,
         }
     ],
     STATICFILES_DIRS=[
@@ -94,10 +51,63 @@ settings.configure(
     STATIC_URL = '/static/'
 )
 
+class ImageForm(forms.Form):
+    height = forms.IntegerField(min_value=1,max_value=2000)
+    width = forms.IntegerField(min_value=1,max_value=2000)
+    def generate(self, image_formate='PNG'):
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        key = '{}.{}.{}'.format(width, height, image_formate)
+        content = cache.get(key) ##增加服务器缓存
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = '{}x{}'.format(width, height)
+            textwidth,textheight = draw.textsize(text)
+            if textwidth < width and textheight < height:
+                texttop = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft,texttop), text, fill=(255,255,255))
+            content = BytesIO()
+            image.save(content, image_formate)
+            content.seek(0)
+            cache.set(key, content, 60*60)
+        return content
+
+##接收
+def generate_etag(req, width, height):
+    content = 'Placeholder: {0}x{1}'.format(width, height)
+    return hashlib.sha1(content.encode('utf-8')).hexdigest()
+
+@etag(generate_etag)
+def placeholder(req, width, height):
+    form = ImageForm({'width':width, 'height':height})
+    if form.is_valid():
+        # height = form.cleaned_data['height']
+        # width = form.cleaned_data['width']
+        image = form.generate()
+        return HttpResponse(image, content_type='image/png')
+    else:
+        return HttpResponseBadRequest('图片格式错误')
+
+def index(req):
+    example = reverse('placeholder', kwargs={'width':50, 'height':50})
+    context = {
+        'example': req.build_absolute_uri(example)
+    }
+    # print ('********',context)
+    return render_to_response('home.html', context)
+
+urlpatterns=[
+    url(u'^$',index, name='homepage'),
+    url(u'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
+]
+
+
+
 application = get_wsgi_application()
 
 if __name__ == "__main__":
-    # print("***********",BASE_DIR)
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
