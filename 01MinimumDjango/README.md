98997118c59b01072bed3d61167b7ad21fa7d6c7
mar <qiyafei_7@yeah.net>
2017-11-16 09:57:13 +0800

    add minimum django into the project

--- 01MinimumDjango/README.md
+++ 01MinimumDjango/README.md
@@ -2,14 +2,17 @@
 This *.py contains what the minimum django needs
 1. setting.py
 2. urls.py
-3. views.
+3. views.py
 4. WSGI service( recommend gunicorn on mac or linux)
-5. cache
 
 Run by
 
     python templateHello.py runserver 0.0.0.0:8000
 
-if you have installed Gunicorn, you can also run it by
+if you have installed Gunicorn(recommend to use on linux or mac, ##pip install gunicorn), you can also run it by
 
     gunicorn -w 6 -b 0.0.0.0:8000 templateHello --log-file=-
+    
+Use templateHello as a minimum template, and create a new django project by :
+
+    django-admin.py startproject foo --template=templateHello 
---
+++ 01MinimumDjango/templateHello/templateHello.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-11-13 21:39:26
+# @Author  : Yafei (qiyafei_7@yeah.net)
+# @Link    : http://www.cnblogs.com/mar-q/
+# @Version : $Id$
+
+import os
+import sys
+from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+
+def index(req):
+	return HttpResponse('HelloWorld')
+
+urlpatterns=[
+	url(u'^$',index),
+]
+
+DEBUG = os.environ.get('DEBUG','on')=='on'
+SECRET_KEY = os.environ.get('SECRET_KEY','{{secret_key}}')
+ALLOWED_HOSTS=['*']
+settings.configure(
+	DEBUG=DEBUG,
+	SECRET_KEY=SECRET_KEY,
+	ROOT_URLCONF=__name__,
+	ALLOWED_HOSTS=ALLOWED_HOSTS,
+	MIDDLEWARE=[
+		'django.middleware.common.CommonMiddleware',
+		'django.middleware.csrf.CsrfViewMiddleware',
+		'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	]
+)
+
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+	from django.core.management import execute_from_command_line
+	execute_from_command_line(sys.argv)
